ccc471b513aeff9ff2039bd72eda4e4905b07e40
tomas <josetomashervas@gmail.com>
2023-01-26 17:27:11 +0100

    middleware

--- middleware.js
+++ middleware.js
@@ -4,23 +4,21 @@ import { jwtVerify, SignJWT } from "jose";
 export async function middleware(request) {
   const token = request.cookies.get("tokenAuth");
   //console.log(token)
-
-  if (request.nextUrl.pathname === "/dashboard") {
-
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    try {
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-      const { payload } = await jwtVerify(token.value, secret);
-      console.log(payload);
-      return NextResponse.next();
-      
-    } catch (error) {
-      console.error(error);
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  return NextResponse.next();
+
+  try {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const { payload } = await jwtVerify(token.value, secret);
+    console.log(payload);
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
+
+export const config = {
+  matcher: ["/dashboard"/* , "admin" */]
+};
